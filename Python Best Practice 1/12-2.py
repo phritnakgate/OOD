fdcbb57a13797dd3e801c3c1e608b0ce2c7602dc
Phrit Nakgate <boss5500216@gmail.com>
2023-07-09 11:19:13 +0700

    Update 12-2.py

--- Python Best Practice 1/12-2.py
+++ Python Best Practice 1/12-2.py
@@ -4,25 +4,22 @@ num = int(input("Enter a positive number : "))
 if num <= 0:
     print(str(num) + " is OUT of range !!!")
 else:
-    divi = []
+    result = [1, num]
     if num == 1:
         print("Output ==> 1")
         print("Total ==> 1")
     else:
-        divi.extend([1, num])
         i = 2
         while i != num:
-            if num % i == 0 and int(i) not in divi:
-                divi.insert(1, i)
-                if int(i) not in divi:
-                    divi.insert(1, num / i)
+            if num % i == 0 and int(i) not in result:
+                result.append(i)
                 i += 1
             else:
                 i += 1
-        divi.sort()
+        result.sort()
         l_num = ""
-        for i in range(len(divi)):
-            l_num += str(divi[i])
+        for i in result:
+            l_num += str(i)
             l_num += " "
         print("Output ==> "+ l_num)
-        print("Total ==> "+str(len(divi)))
+        print("Total ==> "+str(len(result)))
