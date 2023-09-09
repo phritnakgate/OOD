4e9c5c40037852c8dc03cbc71384972eb57100c7
Phrit Nakgate <boss5500216@gmail.com>
2023-09-09 10:59:32 +0700

    Add files via upload

---
+++ 7-1.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return str(self.data)
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        self.root = self._insert(self.root, data)
+        return self.root
+
+    def _insert(self, root, data):
+        if root is None:
+            return Node(data)
+        else:
+            if data < root.data:
+                root.left = self._insert(root.left, data)
+            else:
+                root.right = self._insert(root.right, data)
+        return root
+
+    def printTree(self, node, level=0):
+        if node is not None:
+            self.printTree(node.right, level + 1)
+            print('     ' * level, node)
+            self.printTree(node.left, level + 1)
+
+
+T = BST()
+inp = [int(i) for i in input('Enter Input : ').split()]
+for i in inp:
+    root = T.insert(i)
+
+T.printTree(root)
